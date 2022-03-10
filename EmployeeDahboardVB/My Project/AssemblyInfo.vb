3f2b0f7c65664abd430cd50a369514982688f4f7
likhitachenna <78203319+likhitachenna@users.noreply.github.com>
2022-03-10 11:29:13 +0530

    Add files via upload

---
+++ EmployeeDahboardVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EmployeeForm")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Symphony Retail AI")>
+<Assembly: AssemblyProduct("EmployeeForm")>
+<Assembly: AssemblyCopyright("Copyright © Symphony Retail AI 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f46c0c01-71c5-4bbe-8ae4-cacba0d8e024")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
